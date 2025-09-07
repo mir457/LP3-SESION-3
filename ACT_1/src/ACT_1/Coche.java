07c906d3bc22ac6a146a297b1a535838d4fa6b1f
Francisco0125 <jose.limaf@estudiante.ucsm.edu.pe>
2025-09-06 20:03:54 -0500

    Add files via upload

---
+++ ACT_1/src/ACT_1/Coche.java
@@ -0,0 +1,8 @@
+package ACT_1;
+
+public class Coche implements Vehiculo{
+	@Override
+	public void acelerar() {
+		System.out.println("ACERANDO CON EL MOTOR");
+	}
+}
---
+++ ACT_1/src/ACT_1/Vehiculo.java
@@ -0,0 +1,5 @@
+package ACT_1;
+
+public interface Vehiculo {
+	public void acelerar();
+}
